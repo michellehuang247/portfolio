f081bd735aa86a3dcf3779bf53256ca1e9742a63
michellehuang247 <huangmichelle247@gmail.com>
2017-07-11 10:06:57 -0700

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,16 @@
+list = ["cookies",5,"mangoes","gummy bears","pig",2, "hippo"]
+
+##looks into the list, pulls the actual values, not the index
+##for item in list:
+#    print(item)
+
+##(len(list)) finds the length of this list (which is 7 in this case)
+##because of what we put in as the range, it will only print out numbers
+for item in range(len(list)):
+    #this prints the index numbers
+    #print(item)
+    #this prints each value
+    print (list[item])
+
+##this prints a certain item
+#print (list[3])
